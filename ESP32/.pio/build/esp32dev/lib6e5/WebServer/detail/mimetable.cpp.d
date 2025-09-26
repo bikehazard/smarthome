b435bf6dd7e013df3a41b89129bd5aa2bec59604
Marco <bikehazard@gmail.com>
2025-09-26 15:07:28 +0200

    Remove build objects

--- ESP32/.pio/build/esp32dev/lib6e5/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib6e5/WebServer/detail/mimetable.cpp.o: \
- C:/Users/Marco/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/Marco/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/Marco/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
